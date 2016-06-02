cb6811673d105ddb482914e62a39a0f6f9ada68b
Valmir Memeti <valmirmemeti@gmail.com>
2016-06-02 17:29:46 +0200

    Add new custom list adapter and xml file for showing images

---
+++ app/src/main/java/no/woact/stud/smaola14memval14/pokemonfinder/CustomListAdapter.java
@@ -0,0 +1,44 @@
+package no.woact.stud.smaola14memval14.pokemonfinder;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Valmir on 02.06.2016.
+ */
+public class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final ArrayList<String> itemname;
+    private final ArrayList<Bitmap> imgid;
+
+    public CustomListAdapter(Activity context, ArrayList<String> itemname, ArrayList<Bitmap> imgid) {
+        super(context, R.layout.mylist, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.mylist, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.item);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+
+        txtTitle.setText(itemname.get(position));
+        imageView.setImageBitmap(imgid.get(position));
+        return rowView;
+
+    };
+}
--- app/src/main/java/no/woact/stud/smaola14memval14/pokemonfinder/PokemonOverview.java
+++ app/src/main/java/no/woact/stud/smaola14memval14/pokemonfinder/PokemonOverview.java
@@ -1,13 +1,20 @@
 package no.woact.stud.smaola14memval14.pokemonfinder;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class PokemonOverview extends AppCompatActivity {
@@ -15,20 +22,49 @@ public class PokemonOverview extends AppCompatActivity {
     ListView listViewPokemons;
     ListAdapter adapter;
     ArrayList<String> listPokemons;
+    ArrayList<Bitmap> imgid;
+    ArrayList<String> itemname;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemon_overview);
+
+        /*imgid.add(generateImage("http://vignette4.wikia.nocookie.net/pokemon/images/5/5f/025Pikachu_OS_anime_11.png/revision/latest?cb=20150717063951"));
+        itemname.add("JAMIACABOY");
+
+        CustomListAdapter cAdapter = new CustomListAdapter(this, itemname, imgid );
+        listViewPokemons = (ListView) findViewById(R.id.listPokemons);
+        listViewPokemons.setAdapter(cAdapter);*/
+
+
+
+
+        imgid = new ArrayList<>();
+        itemname = new ArrayList<>();
         db = new DbHandler(this);
         listPokemons = new ArrayList<>();
         for(Pokemon pokemon : db.getPokemonsFromDb()){
-            listPokemons.add("Pokemon: " + pokemon.getName());
+            listPokemons.add("Pokemon: " + pokemon.getName() + pokemon.getImage());
+            imgid.add(generateImage(pokemon.getImage()));
+            itemname.add(pokemon.getName());
         }
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listPokemons);
+        CustomListAdapter cAdapter = new CustomListAdapter(this, itemname, imgid );
         listViewPokemons = (ListView) findViewById(R.id.listPokemons);
-        listViewPokemons.setAdapter(adapter);
+        listViewPokemons.setAdapter(cAdapter);
+
+    }
+
+    public Bitmap generateImage(String imageUrl){
+        try {
+            return BitmapFactory.decodeStream((InputStream)new URL(imageUrl).getContent());
+        } catch (MalformedURLException m) {
+            m.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
 
     }
 }
